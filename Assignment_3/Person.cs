ccd2515f0cebd113c3bcf16516bbb1a630da15e8
Roshan Poudel <rpoudel912@gmail.com>
2023-01-27 11:16:41 -0700

    initial commit

---
+++ Assignment_3/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    internal class Person : BaseClass
+    {
+        public string FirstName {get;set;}
+        public string LastName { get;set;}
+        public Person(string firstName="", string lastName="")
+        {
+            FirstName= firstName;
+            LastName= lastName;
+
+        }
+
+        public override Boolean IsValidated()
+        {
+           FirstName =  FirstName ?? "";
+           LastName = LastName ?? "";
+
+            if((FirstName.Length > 0) && (LastName.Length > 0)){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Assignment_3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bob = new Person();
+            Console.WriteLine(bob.IsValidated());
+
+            Person kim = new Person("kim","smith");
+            Console.WriteLine(kim.IsValidated());
+        }
+    }
+}
